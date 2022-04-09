7653b81e3b587844fb6680bbd21e8bb1f74f0a69
DavidMarkovich1218 <87656402+DavidMarkovich1218@users.noreply.github.com>
2022-04-09 18:35:26 +0300

    I added a Constants object, where I put all the data. Added "security". Now, each employee has his own unique code to confirm his identity.

--- app/src/main/java/com/example/getpaycheck/MainActivity.kt
+++ app/src/main/java/com/example/getpaycheck/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.getpaycheck.constances.Constatnce
 import com.example.getpaycheck.databinding.ActivityMainBinding
 
 
@@ -16,31 +17,78 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.getBtn.setOnClickListener(){
+        binding.getBtn.setOnClickListener() {
             val inputName = binding.inputNameEdit.text.toString()
-            when(inputName){
-                "Alex" -> {
-                        binding.getResult.visibility = View.VISIBLE
-                        binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                        binding.getResult.text = inputName+", Get 1000$ paycheck"
+            val inputCode = binding.inputCodeEdit.text.toString()
+            when (inputName) {
+                Constatnce.BACKEND -> {
+                    binding.inputCode.visibility = View.VISIBLE
+                    when (inputCode ) {
+                        Constatnce.BACKEND_CODE -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                            val tempText = ", Get ${Constatnce.BACKEND_PAYCHECK}$ paycheck"
+                            binding.getResult.text = inputName + tempText
+                        }
+                        else -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                            binding.getResult.text = "You enter wrong code. Please Try Again."
+                        }
                     }
-                "Nikita" -> {
-                    binding.getResult.visibility = View.VISIBLE
-                    binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                    binding.getResult.text = inputName+", Get 900$ paycheck"
+
                 }
-                "Vasiliy" -> {
-                    binding.getResult.visibility = View.VISIBLE
-                    binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                    binding.getResult.text = inputName+", Get 875$ paycheck"
+                Constatnce.FRONTEND -> {
+                    binding.inputCode.visibility = View.VISIBLE
+                    when (inputCode ) {
+                        Constatnce.FRONTEND_CODE -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                            val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
+                            binding.getResult.text = inputName + tempText
+                        }
+                        else -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                            binding.getResult.text = "You enter wrong code. Please Try Again."
+                        }
+                    }
                 }
-                "David" -> {
-                    binding.getResult.visibility = View.VISIBLE
-                    binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                    binding.getResult.text = inputName+", Get 1230$ paycheck"
+                Constatnce.FULLSTACK -> {
+                    binding.inputCode.visibility = View.VISIBLE
+                    when (inputCode ) {
+                        Constatnce.FULLSTACK_CODE -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                            val tempText = ", Get ${Constatnce.FULLSTACK_PAYCHECK}$ paycheck"
+                            binding.getResult.text = inputName + tempText
+                        }
+                        else -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                            binding.getResult.text = "You enter wrong code. Please Try Again."
+                        }
+                    }
+                }
+                Constatnce.NATIVE -> {
+                    binding.inputCode.visibility = View.VISIBLE
+                    when (inputCode ) {
+                        Constatnce.NATIVE_CODE -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                            val tempText = ", Get ${Constatnce.NATIVE_PAYCHECK}$ paycheck"
+                            binding.getResult.text = inputName + tempText
+                        }
+                        else -> {
+                            binding.getResult.visibility = View.VISIBLE
+                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                            binding.getResult.text = "You enter wrong code. Please Try Again."
+                        }
+                    }
                 }
                 else -> {
                     binding.getResult.visibility = View.VISIBLE
+                    binding.inputCode.visibility = View.INVISIBLE
                     binding.getResult.setTextColor(Color.parseColor("#D36152"))
                     binding.getResult.text = "Your name was not found in the payroll database."
                 }
---
+++ app/src/main/java/com/example/getpaycheck/constances/Constatnce.kt
@@ -0,0 +1,19 @@
+package com.example.getpaycheck.constances
+
+object Constatnce {
+    const val BACKEND_PAYCHECK = 1000
+    const val FRONTEND_PAYCHECK = 900
+    const val FULLSTACK_PAYCHECK = 875
+    const val NATIVE_PAYCHECK = 1230
+
+    const val BACKEND = "Alex"
+    const val FRONTEND = "Nikita"
+    const val FULLSTACK = "Vasiliy"
+    const val NATIVE = "David"
+
+    const val BACKEND_CODE = "341"
+    const val FRONTEND_CODE = "237"
+    const val FULLSTACK_CODE = "982"
+    const val NATIVE_CODE = "14031"
+
+}
