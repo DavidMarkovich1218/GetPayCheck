91c36260efc4aee552fa7aad262623cbaa9abb30
DavidMarkovich1218 <87656402+DavidMarkovich1218@users.noreply.github.com>
2022-04-10 13:07:03 +0300

    Replaced when(){when(){}} nesting with when(){if(){}else{}}

--- app/src/main/java/com/example/getpaycheck/MainActivity.kt
+++ app/src/main/java/com/example/getpaycheck/MainActivity.kt
@@ -22,73 +22,61 @@ class MainActivity : AppCompatActivity() {
             val inputCode = binding.inputCodeEdit.text.toString()
             when (inputName) {
                 Constatnce.BACKEND -> {
-                    binding.inputCode.visibility = View.VISIBLE
-                    when (inputCode ) {
-                        Constatnce.BACKEND_CODE -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                            val tempText = ", Get ${Constatnce.BACKEND_PAYCHECK}$ paycheck"
-                            binding.getResult.text = inputName + tempText
-                        }
-                        else -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
-                            binding.getResult.text = "You enter wrong code. Please Try Again."
-                        }
+                    if(binding.inputCodeEdit.text.toString() == Constatnce.BACKEND_CODE) {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                        val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
+                        binding.getResult.text = inputName + tempText
+                    }
+                    else {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                        binding.getResult.text = "You enter wrong code. Please Try Again."
                     }
 
                 }
                 Constatnce.FRONTEND -> {
-                    binding.inputCode.visibility = View.VISIBLE
-                    when (inputCode ) {
-                        Constatnce.FRONTEND_CODE -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                            val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
-                            binding.getResult.text = inputName + tempText
-                        }
-                        else -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
-                            binding.getResult.text = "You enter wrong code. Please Try Again."
-                        }
+                    if(binding.inputCodeEdit.text.toString() == Constatnce.FRONTEND_CODE) {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                        val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
+                        binding.getResult.text = inputName + tempText
+                    }
+                    else {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                        binding.getResult.text = "You enter wrong code. Please Try Again."
                     }
                 }
                 Constatnce.FULLSTACK -> {
-                    binding.inputCode.visibility = View.VISIBLE
-                    when (inputCode ) {
-                        Constatnce.FULLSTACK_CODE -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                            val tempText = ", Get ${Constatnce.FULLSTACK_PAYCHECK}$ paycheck"
-                            binding.getResult.text = inputName + tempText
-                        }
-                        else -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
-                            binding.getResult.text = "You enter wrong code. Please Try Again."
-                        }
+                    if(binding.inputCodeEdit.text.toString() == Constatnce.FULLSTACK_CODE) {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                        val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
+                        binding.getResult.text = inputName + tempText
+                    }
+                    else {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                        binding.getResult.text = "You enter wrong code. Please Try Again."
                     }
                 }
                 Constatnce.NATIVE -> {
                     binding.inputCode.visibility = View.VISIBLE
-                    when (inputCode ) {
-                        Constatnce.NATIVE_CODE -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#5CB643"))
-                            val tempText = ", Get ${Constatnce.NATIVE_PAYCHECK}$ paycheck"
-                            binding.getResult.text = inputName + tempText
-                        }
-                        else -> {
-                            binding.getResult.visibility = View.VISIBLE
-                            binding.getResult.setTextColor(Color.parseColor("#D36152"))
-                            binding.getResult.text = "You enter wrong code. Please Try Again."
-                        }
+                    if(binding.inputCodeEdit.text.toString() == Constatnce.NATIVE_CODE) {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#5CB643"))
+                        val tempText = ", Get ${Constatnce.FRONTEND_PAYCHECK}$ paycheck"
+                        binding.getResult.text = inputName + tempText
+                    }
+                    else {
+                        binding.getResult.visibility = View.VISIBLE
+                        binding.getResult.setTextColor(Color.parseColor("#D36152"))
+                        binding.getResult.text = "You enter wrong code. Please Try Again."
                     }
                 }
                 else -> {
                     binding.getResult.visibility = View.VISIBLE
-                    binding.inputCode.visibility = View.INVISIBLE
                     binding.getResult.setTextColor(Color.parseColor("#D36152"))
                     binding.getResult.text = "Your name was not found in the payroll database."
                 }
